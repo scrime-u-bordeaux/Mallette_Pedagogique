43e66932b1f0a20f19d7f693bcc2380d618d45ca
clement-baby <85685313+clement-baby@users.noreply.github.com>
2021-07-19 10:25:21 +0200

    Update touch.ino

--- Contrôleurs/touch/touch.ino
+++ Contrôleurs/touch/touch.ino
@@ -1,8 +1,8 @@
-int pin[8] = {0, 1, 3, 4, 15, 16, 17, 18};
-int touch[8] = {0};
-int lastTouch[8] = {0};
-int calib[8] = {0};
-int midiCC[8] = {0};
+int pin[10] = {0, 1, 3, 4, 15, 16, 17, 18, 19, 22};
+int touch[10] = {0};
+int lastTouch[10] = {0};
+int calib[10] = {0};
+int midiCC[10] = {0};
 
 void setup() {
   Serial.begin(9600);
@@ -10,16 +10,22 @@ void setup() {
   pinMode(1, INPUT);
   pinMode(3, INPUT);
   pinMode(4, INPUT);
+  pinMode(15, INPUT);
+  pinMode(16, INPUT);
+  pinMode(17, INPUT);
+  pinMode(18, INPUT);
+  pinMode(19, INPUT);
+  pinMode(22, INPUT);
   calibration();
 }
 
 void loop() {
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 10; i++) {
     touch[i] = touchRead(pin[i]);
     touch[i] = touch[i] * 0.9 + lastTouch[i] * 0.1;
     lastTouch[i] = touch[i];
   }
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 10; i++) {
     midiCC[i] = (touch[i] - 1.1 * calib[i]) * 255 / calib[i];
     midiCC[i] = constrain( midiCC[i], 0, 127);
     usbMIDI.sendControlChange(101 + i, midiCC[i], 1); 
@@ -33,12 +39,12 @@ void loop() {
 
 void calibration() {
   for (int i = 0; i < 100; i++) {
-    for (int j = 0; j < 8; j++) {
+    for (int j = 0; j < 10; j++) {
       calib[j] = calib[j] + touchRead(pin[j]);
     }
     delay(10);
   }
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 10; i++) {
     calib[i] = calib[i] / 100;
   }
 }
