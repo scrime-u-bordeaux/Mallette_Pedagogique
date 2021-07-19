d488e2c88b2c581ee75d5a4db3c8c5f2d59b06e5
clement-baby <85685313+clement-baby@users.noreply.github.com>
2021-07-19 10:53:40 +0200

    Update analogiques.ino

--- Contrôleurs/analogiques/analogiques.ino
+++ Contrôleurs/analogiques/analogiques.ino
@@ -1,27 +1,36 @@
 //#define DEBUG
 
-int pinRead = A0;
+int pinRead[9] = {A0, A1, A2, A3, A4, A5, A6, A7, A8};
+int valRead[9] = {0};
+int valMSB[9] = {0};
+int valLSB[9] = {0};
 
 void setup() {
   Serial.begin(9600);
-  pinMode(pinRead, INPUT);
+  for (int i = 0; i < 9; i++) {
+    pinMode(pinRead[i], INPUT);
+  }
   analogReadResolution(14);
 }
 
 void loop() {
-  uint16_t valRead = analogRead(pinRead);
-  uint16_t valMSB = valRead >> 7;
-  uint16_t valLSB = valRead & 127;
+  for (int i = 0; i < 9; i++) {
+    uint16_t valRead[i] = analogRead(pinRead[i]);
+    uint16_t valMSB[i] = valRead[i] >> 7;
+    uint16_t valLSB[i] = valRead[i] & 127;
+    usbMIDI.sendControlChange(1 + i, valMSB, 1);
+    usbMIDI.sendControlChange(33 + i, valLSB, 1);
+  }
 
-  usbMIDI.sendControlChange(1, valMSB, 1);
-  usbMIDI.sendControlChange(33, valLSB, 1);
   delay(20);
 
 #ifdef DEBUG
-  Serial.print(valMSB, DEC);
-  Serial.print("\t");
-  Serial.print(valLSB, DEC);
+  for (int i = 0; i < 9; i++) {
+    Serial.print(valMSB[i], DEC);
+    Serial.print("\t");
+    Serial.print(valLSB[i], DEC);
+    Serial.print("\t");
+  }
   Serial.println();
-  delay(50);
 #endif
 }
